73dd27c9b71f3458404971433a1acf0b27a8594a
BurakBlm <burakbulama5454@gmail.com>
2024-05-08 23:04:11 +0300

    generate funciton chat properties

--- generate.py
+++ generate.py
@@ -17,9 +17,8 @@ tokenizer = get_tokenizer()
 model_path = os.getcwd() + "/model/snapshot.pt"
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-text = "<user>merhabalar<bot>merhaba bu gün size nasıl yardımcı olabilirim<user>Fırat üniversitesinde kaç adet bölüm vardır?<bot>"
 
-def generate_text(model, text: str, stop_token, max_token: int = 100, temprature: float = 1.0):
+def generate_text(model, text: str, stop_token=[32000], max_token: int = 100, temprature: float = 1.0):
     model.eval()
     idx = torch.tensor([tokenizer.encode(text)], dtype=torch.long, device=device)
 
@@ -29,12 +28,20 @@ def generate_text(model, text: str, stop_token, max_token: int = 100, temprature
             logits = logits[:, -1, :] / temprature
             props = F.softmax(logits, dim=-1)
             idx_next = torch.multinomial(props, num_samples=1)
-            print(idx_next)
             if idx_next in stop_token:
                 break
             idx = torch.cat((idx, idx_next), dim=1)
             
     return tokenizer.decode(idx[0].tolist())
 
-a = generate_text(model, text, stop_token=[32001])
-print(a)
+
+
+text = ""
+
+for i in range(5):
+    inpt = input("Human: ")
+    inpt = "<user>" + inpt + "<bot>"
+    response = generate_text(model, inpt)
+    text = response
+    print("Bot: ",response)
+
